89255da84ac717d34c2425fd1c848e759211f64b
Lars Drechsler <lars.d@mailbox.org>
2025-05-13 22:59:20 +0200

    fix: Handle Dryad datasets that cannot be downloaded as single zip files

--- geoextent/lib/content_providers/Dryad.py
+++ geoextent/lib/content_providers/Dryad.py
@@ -1,5 +1,4 @@
-import math
-import time
+from pathlib import Path
 from requests import HTTPError
 import urllib.parse
 from .providers import DoiProvider
@@ -30,47 +29,64 @@ class Dryad(DoiProvider):
             return False
 
     def _get_metadata(self):
-        # ! DEPRECATED function
         if self.validate_provider:
-            # TODO: except http error and retry
             try:
                 resp = self._request(
-                    "{}{}".format(self.host["api"], self.record_id_html), headers={"accept": "application/json"}
+                    "{}{}".format(
+                        self.host["api"],
+                        self.record_id_html,
+                    ),
+                    headers={"accept": "application/json"},
+                    throttle=self.throttle,
                 )
-                resp.raise_for_status()
                 self.record = resp.json()
-                return self.record
             except Exception:
                 m = "The Dryad dataset : " + self.get_url + " does not exist"
                 self.log.warning(m)
                 raise HTTPError(m)
+
+            latest_version = self.record["_links"]["stash:version"]["href"]
+
+            try:
+                resp = self._request(
+                    "{}{}{}".format(
+                        "https://datadryad.org",
+                        latest_version,
+                        "/files",
+                    ),
+                    headers={"accept": "application/json"},
+                    throttle=self.throttle,
+                )
+                self.record2 = resp.json()
+            except Exception as e:
+                # TODO: make it prettier
+                print("DEBUG:", e)
+                raise
+
+            return self.record2
+
         else:
             raise ValueError('Invalid content provider')
 
     @property
     def _get_file_links(self):
-        """
         try:
-            self._get_metadata()
-            record = self.record
+            record = self._get_metadata()
         except ValueError as e:
             raise Exception(e)
 
         try:
-            files = record['files']
-        except:
+            files = record['_embedded']["stash:files"]
+        except Exception:
             m = "This record does not have Open Access files. Verify the Access rights of the record."
             self.log.warning(m)
             raise ValueError(m)
 
         file_list = []
         for j in files:
-            file_list.append(j['links']['self'])
-        return file_list
-        """
-        # very simple method for download only, instead of 2+ API queries, but without metadata capabilities
-        file_list = []
-        file_list.append(self.host["api"] + self.record_id_html + "/download")
+            link = "https://datadryad.org" + j["_links"]["stash:download"]["href"]
+            path = j["path"]
+            file_list.append([link, path])
         return file_list
 
 
@@ -78,22 +94,39 @@ class Dryad(DoiProvider):
         self.throttle = throttle
         self.log.debug("Downloading Dryad dataset id: {} ".format(self.record_id))
         try:
-            download_links = self._get_file_links
+            # very simple method for download only, instead of 2+ API queries, but without metadata capabilities
+            download_links = [self.host["api"] + self.record_id_html + "/download"]
             counter = 1
             for file_link in download_links:
-                resp = self.session.get(file_link, stream=True)
+                resp = self._request(file_link, throttle=self.throttle, stream=True,)
                 filename = "dataset.zip"
                 filepath = os.path.join(folder, filename)
-                try:
-                    with open(filepath, "wb") as dst:
-                        for chunk in resp.iter_content(chunk_size=None):
-                            dst.write(chunk)
-                except Exception as e:
-                    print("EXCEPTION:", e)
-                    m = "The Dryad dataset : " + self.get_url + " does not exist"
-                    self.log.warning(m)
-                    raise HTTPError(m)
-                    print("DEBUG: dryad download error:", self.get_url)
+
+                with open(filepath, "wb") as dst:
+                    for chunk in resp.iter_content(chunk_size=None):
+                        dst.write(chunk)
+
+                self.log.debug("{} out of {} files downloaded.".format(counter, len(download_links)))
+                counter += 1
+        except ValueError as e:
+            raise Exception(e)
+        except HTTPError as e:
+            if e.response.content != b'The dataset is too large for zip file generation. Please download each file individually.':
+                m = "The Dryad dataset : " + self.get_url + " cannot be accessed"
+                self.log.warning(m)
+                raise HTTPError(m)
+
+        try:
+            print("Downloading individial files.")
+            download_links = self._get_file_links
+            counter = 1
+            for file_link, filename in download_links:
+                resp = self._request(file_link, throttle=self.throttle, stream=True,)
+                filepath = Path(folder).joinpath(filename)
+                # TODO: catch http error (?)
+                with open(filepath, "wb") as dst:
+                    for chunk in resp.iter_content(chunk_size=None):
+                        dst.write(chunk)
                 self.log.debug("{} out of {} files downloaded.".format(counter, len(download_links)))
                 counter += 1
         except ValueError as e:
